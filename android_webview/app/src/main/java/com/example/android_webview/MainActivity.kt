e94eedb9949d7fb7e591934ecb102ea547f45cfd
Sunbreak <sunbreak.wang@gmail.com>
2020-12-30 07:38:18 +0800

    Add navigation to android_webview
    Signed-off-by: Sunbreak <sunbreak.wang@gmail.com>

--- android_webview/app/src/main/java/com/example/android_webview/MainActivity.kt
+++ android_webview/app/src/main/java/com/example/android_webview/MainActivity.kt
@@ -1,21 +1,73 @@
 package com.example.android_webview
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.webkit.WebView
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var webview: WebView
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        webview = findViewById(R.id.webview)
-        webview.settings.javaScriptEnabled = true
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
 
-        webview.loadUrl("https://github.com")
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
+    }
+}
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.webView).setOnClickListener {
+            findNavController().navigate(R.id.webView)
+        }
+    }
+}
+
+class WebViewFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_webview, container, false)
+    }
+
+    private lateinit var webView: WebView
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        webView = view.findViewById(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+
+        webView.loadUrl("https://github.com")
     }
 }
